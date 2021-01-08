46f10784a9e92b7bd1dc9536a40d5bd7cf28f1c0
dave vebber <davevebber@gmail.com>
2021-01-08 15:34:03 -0800

    password get generator per prompt request. but if you request a second password it gets added on to the first

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,59 @@
-let specialSymbols = '!@#$%^&*()';
+// variables 
+const lower = ('abcdefghijklmnopqrstuvwxyz').split('');
+const upper = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ').split('');
+const num = ('1234567890').split('');
+const sym = ('!@#$%^&*()').split('');
+let passHolder = [];
+let passwordArr = [];
+
+function generatePassword() {
+  // prompts for how you want your password
+  let passwordLength = prompt('How long would you like your password to be? Please choose between 8 and 128 character.')
+    if (passwordLength < 8 || passwordLength > 128 || passwordLength === '') {
+      alert('Password length must be between 8 and 128 characters')
+    };
+  let lowerCase = confirm('Would you like to use lowercase letters?');
+  let upperCase = confirm('Would you like to use uppercase letters?');
+  let numbers = confirm('Would you like to use numbers?');
+  let symbols = confirm('Would you like to use symbols?');
+    if (lowerCase === false && upperCase === false && numbers ===false && symbols === false) {
+      alert('You must choose at least one variable for your password.')
+    }
+
+  // if statements to determine what characters you want in password
+  if (lowerCase === true) {
+     passHolder = passHolder.concat(lower)
+   } 
+
+   if (upperCase === true) {
+     passHolder = passHolder.concat(upper)
+   }
+
+   if (numbers === true) {
+    passHolder =  passHolder.concat(num)
+  }
+
+  if (symbols === true) {
+    passHolder = passHolder.concat(sym)
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    passwordArr.push(passHolder[Math.floor(Math.random() * passHolder.length)])
+  }
+
+return(passwordArr.join(''));
+};
+
+// Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
- 
-// function to write the password
+
+// Write password to the #password input
 function writePassword() {
   let password = generatePassword();
-  let passwordText = document.querySelector('#password');
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// function to generate the password
-function generatePassword() {
-  let passwordLength = prompt('How long would you like your password? (Between 9 and 128 characters)');
-    if (passwordLength < 9 || passwordLength > 128 || passwordLength === "") {
-      alert('Password length must be between 9 and 128 characters long.')
-    }
-  let lowerCase = confirm('Would you like to use lowercase letters?');
-  let upperCase = confirm('Would you like to use uppercase letters?');
-  let numbers = confirm('Would you like to use numbers?');
-  let symbols = confirm('Would  you like to use special symbols?')
-
- 
-  let minimumCount = 0;
-  let minimumNumbers = "";
-  let minimumLowerCases = "";
-  let minimumUpperCases = "";
-  let minimumSpecialCharacters = "";
-
-  let functionArr = {
-    getNumbers: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-    },
-
-    getLowerCases: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-    },
-
-    getUpperCases: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-    },
-
-    getSpecialCharacters: function() {
-      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
-    }
-
-};
-
-let randomPasswordGenerated = "";
-
-for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
-  let randomNumberPicked = Math.floor(Math.random() * 4);
-
-  randomPasswordGenerated += randomNumberPicked;
-
-}
-
-
-randomPasswordGenerated += minimumNumbers;
-randomPasswordGenerated += minimumLowerCases;
-randomPasswordGenerated += minimumUpperCases;
-randomPasswordGenerated += minimumSpecialCharacters;
-return randomPasswordGenerated;
-
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
