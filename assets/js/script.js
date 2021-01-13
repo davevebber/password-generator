f722f75b366ec772caba0f1d439a3259e6df1565
dave vebber <davevebber@gmail.com>
2021-01-12 18:58:00 -0800

    fixed bug if you choose no variables instead of just stopping with an alert. the  app will run password function again

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,46 +8,45 @@ let passwordArr = [];
 
 function generatePassword() {
   // prompts for how you want your password
-  if (passwordArr.length == 0)
-  {
-      let passwordLength = prompt('How long would you like your password to be? Please choose between 8 and 128 character.')
-        if (passwordLength < 8 || passwordLength > 128 || passwordLength === '') {
-          alert('Password length must be between 8 and 128 characters');
-          return;
-        };
-      let lowerCase = confirm('Would you like to use lowercase letters?');
-      let upperCase = confirm('Would you like to use uppercase letters?');
-      let numbers = confirm('Would you like to use numbers?');
-      let symbols = confirm('Would you like to use symbols?');
-        if (lowerCase === false && upperCase === false && numbers ===false && symbols === false) {
-          alert('You must choose at least one variable for your password.')
-        }
-    
-      // if statements to determine what character types you want in password
-      if (lowerCase === true) {
-        passHolder = passHolder.concat(lower)
-      } 
-    
-      if (upperCase === true) {
-        passHolder = passHolder.concat(upper)
-      }
-    
-      if (numbers === true) {
-        passHolder =  passHolder.concat(num)
-      }
-    
-      if (symbols === true) {
-        passHolder = passHolder.concat(sym)
-      }
-    
-      for (let i = 0; i < passwordLength; i++) {
-        passwordArr.push(passHolder[Math.floor(Math.random() * passHolder.length)])
-      }
-    
-    return(passwordArr.join(''));
-  } 
-  else
-  {
+  if (passwordArr.length == 0) {
+    let passwordLength = prompt('How long would you like your password to be? Please choose between 8 and 128 character.')
+    if (passwordLength < 8 || passwordLength > 128 || passwordLength === '') {
+      alert('Password length must be between 8 and 128 characters');
+      return;
+    };
+    let lowerCase = confirm('Would you like to use lowercase letters?');
+    let upperCase = confirm('Would you like to use uppercase letters?');
+    let numbers = confirm('Would you like to use numbers?');
+    let symbols = confirm('Would you like to use symbols?');
+    if (lowerCase === false && upperCase === false && numbers === false && symbols === false) {
+      alert('You must choose at least one variable for your password.')
+      generatePassword();
+    } 
+
+    // if statements to determine what character types you want in password
+    if (lowerCase === true) {
+      passHolder = passHolder.concat(lower)
+    }
+
+    if (upperCase === true) {
+      passHolder = passHolder.concat(upper)
+    }
+
+    if (numbers === true) {
+      passHolder = passHolder.concat(num)
+    }
+
+    if (symbols === true) {
+      passHolder = passHolder.concat(sym)
+    }
+
+    for (let i = 0; i < passwordLength; i++) {
+      passwordArr.push(passHolder[Math.floor(Math.random() * passHolder.length)])
+    }
+
+    return (passwordArr.join(''));
+  }
+  else {
     alert("please refresh page for new password")
   }
 };
@@ -58,13 +57,11 @@ let generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
-  if (password == undefined)
-  {
-  } 
-  else
-  {
+  if (password == undefined) {
+  }
+  else {
     let passwordText = document.querySelector("#password");
-  
+
     passwordText.value = password;
   }
 }
